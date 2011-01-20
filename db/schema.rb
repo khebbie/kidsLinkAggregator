d1107e2b7cf0c873b463c182e5a4635c4e461068
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-01-20 19:19:36 +0100

    added admin role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110116092327) do
+ActiveRecord::Schema.define(:version => 20110120181811) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(:version => 20110116092327) do
   add_index "links", ["category_id"], :name => "index_links_on_category_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20110116092327) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
