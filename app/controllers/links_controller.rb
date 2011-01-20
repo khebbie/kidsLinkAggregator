98febbc452fd29c1b27103d0499b59ffc87ad753
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-01-20 19:26:12 +0100

    not allowed to edit links if not administrator

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -44,6 +44,9 @@ class LinksController < ApplicationController
 
   # GET /links/1/edit
   def edit
+    if !current_user || !current_user.admin?
+      redirect_to :root
+    end
     @link = Link.find(params[:id])
   end
 
