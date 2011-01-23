c338feeb730bfe738d4441ef989bbc9e15d64eea
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-01-23 16:58:17 +0100

    javascript refactoring

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,3 +1,18 @@
+function populateCategory(catid, approvedLinks)
+{
+   $('.result').append('<h3>' + catid.category.name + '</h3>');
+   $('.result').append('<p><ul>');
+
+   $.each(approvedLinks, function(index, value) {
+     if(value.link.category_id == catid.category.id)
+     {
+       $('.result').append('<li><a href="' + value.link.url + '" target="_blank">' + value.link.name + '</a></li>');
+     }
+     $('.result').append('</ul></p>');
+    });
+
+}
+
 function populateLinks()
 {
   var approvedLinks;
@@ -5,21 +20,12 @@ function populateLinks()
  $.getJSON('/approved.json', function(app) {
    approvedLinks = app;
  });
+
  $.getJSON('/categories.json', function(categories) {
-
-      jQuery.each(categories, function(i, catid) {
-        $('.result').append('<h3>' + catid.category.name + '</h3>');
-       
-        $('.result').append('<p><ul>');
-
-        $.each(approvedLinks, function(index, value) {
-         
-         if(value.link.category_id == catid.category.id)
-          
-          $('.result').append('<li><a href="' + value.link.url + '" target="_blank">' + value.link.name + '</a></li>');
-            
-        $('.result').append('</ul></p>');
-          });
-      }); 
-      });
+    jQuery.each(categories, function(i, catid) {
+      populateCategory(catid, approvedLinks);
+    }); 
+  });
 }
+
+
