8045dd1e908ed0260edaf4d9bdacd21de3cdd157
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-01-23 18:46:17 +0100

    added a bit more validation to link model2

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :Category
-  validates_presence_of :name
+  validates_presence_of :name, :url, :category_id
   validates_format_of :url, :with => /^(http|https):\/\/.*\.[a-zA-Z]{2,3}/
   scope :approved, where("approved = ?", true)
 
