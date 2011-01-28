75c1b0dd063717d450782b563e7c475841d30aa1
Klaus Hebsgaard <klaus@hebsgaard.dk>
2011-01-28 23:02:07 +0100

    jslint

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,31 +1,28 @@
-function populateCategory(catid, approvedLinks)
-{
-   $('.result').append('<h3>' + catid.category.name + '</h3>');
-   $('.result').append('<p><ul>');
+function populateCategory(catid, approvedLinks) {
+    $('.result').append('<h3>' + catid.category.name + '</h3>');
+    $('.result').append('<p><ul>');
 
-   $.each(approvedLinks, function(index, value) {
-     if(value.link.category_id == catid.category.id)
-     {
-       $('.result').append('<li><a href="' + value.link.url + '" target="_blank">' + value.link.name + '</a></li>');
-     }
-     $('.result').append('</ul></p>');
+    $.each(approvedLinks, function (index, value) {
+        if (value.link.category_id === catid.category.id) {
+            $('.result').append('<li><a href="' + value.link.url + '" target="_blank">' + value.link.name + '</a></li>');
+        }
+        $('.result').append('</ul></p>');
     });
 
 }
 
-function populateLinks()
-{
-  var approvedLinks;
-  //http://stackoverflow.com/questions/2465446/getjson-each-returns-undefined
- $.getJSON('/approved.json', function(app) {
-   approvedLinks = app;
+function populateLinks() {
+    var approvedLinks;
+    //http://stackoverflow.com/questions/2465446/getjson-each-returns-undefined
+    $.getJSON('/approved.json', function (app) {
+        approvedLinks = app;
 
- $.getJSON('/categories.json', function(categories) {
-    jQuery.each(categories, function(i, catid) {
-      populateCategory(catid, approvedLinks);
-    }); 
-  });
-  });
+        $.getJSON('/categories.json', function (categories) {
+            jQuery.each(categories, function (i, catid) {
+                populateCategory(catid, approvedLinks);
+            }); 
+        });
+    });
 }
 
 
